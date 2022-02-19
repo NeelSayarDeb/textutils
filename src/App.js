d5930db9aa35a0d777b69206e6356a5c17ce5982
Neel <deb.neelsayar@gmail.com>
2022-02-19 14:40:26 +0530

    alerts

--- src/App.js
+++ src/App.js
@@ -2,26 +2,46 @@
 import React, {useState} from 'react'
 import './App.css';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 function App() {
   const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+  const [btnType, setbtnType] = useState('primary')
+  
+
+  const showAlert= (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
   const toggleMode = () => {
     if(mode === 'light'){
       setMode('dark')
       document.body.style.backgroundColor = 'black'
       document.body.style.color = 'white'
+      showAlert("Dark Mode set ","secondary")
+      setbtnType('secondary')
+
     } else {
       setMode('light')
       document.body.style.backgroundColor = 'white'
       document.body.style.color = 'black'
+      showAlert("Light Mode set ","info")
+      setbtnType('primary')
     }
   }
   return (
     <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container"> 
-        <Textform heading="Enter your text to analyze" mode={mode}/>
+        <Textform heading="Enter your text to analyze" mode={mode} btnType={btnType} showAlert={showAlert}/>
         {/* <About/> */}
       </div>
     </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -7,12 +7,15 @@ export default function Textform(props) {
     }
     const handleUpClick = () => {
         setText(text.toUpperCase())
+        props.showAlert("Converted to UpperCase","success")
     }
     const handleDownClick = () => {
         setText(text.toLowerCase())
+        props.showAlert("Converted to LowerCase","success")
     }
     const handleClearClick = () => {
         setText('')
+        props.showAlert("Cleared","success")
     }
     const [text, setText] = useState('');
   return (
@@ -22,9 +25,9 @@ export default function Textform(props) {
                 <h3 htmlFor="myBox" className="form-h3 my-2">{props.heading}</h3>
                 <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange} value={text} style={{background: props.mode === 'light' ? 'white' : 'grey'}}></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleDownClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+            <button className={`btn btn-${props.btnType}`} onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className={`btn btn-${props.btnType} mx-1`} onClick={handleDownClick}>Convert to Lowercase</button>
+            <button className={`btn btn-${props.btnType} mx-1`} onClick={handleClearClick}>Clear Text</button>
         </div> 
         <div className="container my-3">
             <h1>Your Word summary</h1>
